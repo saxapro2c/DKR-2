928fa0330fae75f38b8b205001c42c5402a2fc93
saxapro2c <114305785+saxapro2c@users.noreply.github.com>
2022-11-29 08:18:35 +0300

    Add files via upload

---
+++ КР2-1.pas
@@ -0,0 +1,22 @@
+﻿begin
+  var k, n, min, a: integer;
+  var mx: array of integer;
+  read(n);
+  SetLength(mx, n);
+  for var i := 0 to n - 1 do
+  begin
+    read(a);
+    mx[k] := a;
+    inc(k);
+  end;
+  k := 0;
+  min := mx[1];
+  for var i := 0 to n - 1 do
+    if mx[i] < min then
+      min := mx[i];
+  for var i := 0 to n - 1 do
+    if mx[i] = min then
+      inc(k);
+  writeln('Кол-во мин элемента: ', k);
+  writeln('min элем: ', min);
+end.
---
+++ КР2-2.pas
@@ -0,0 +1,29 @@
+﻿var    i,n:integer;
+begin
+println('Введите строку содержащую несколько одинаковых подстрок');
+var s:= readstring;
+println('Введите подстроку для поиска и удаления');
+var s1:= readstring;
+s+=' ';
+n:=0;
+i:=1;
+while i<=length(s) do
+if s[i]=s1[1] then
+ begin
+  var j:integer;
+  j:=1;
+  while(j<=length(s1))and(s[i+j-1]=s1[j])do inc(j);
+  if j>length(s1) then inc(n);
+  if n mod 2=0 then
+   begin
+    for j:=1 to length(s1) do
+     begin
+      for var k:=i to length(s)-1 do
+      s[k]:=s[k+1];     
+     end;
+   end
+  else i:=i+1;
+ end
+else i:=i+1;
+writeln(s)
+end.
